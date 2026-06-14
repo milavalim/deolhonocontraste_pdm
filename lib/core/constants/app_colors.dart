365472c7269254cc65972074e66ed7f03c3e8d4b
Camila Valim Dias <camila.valim2004@gmail.com>
2026-06-14 17:16:55 -0300

    Task: criando constantes das cores que vou usar no app.

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+/// Classe que centraliza as cores utilizadas no aplicativo.
+/// Segue as diretrizes de design minimalista com tons de azul petróleo e amarelo claro.
+class AppColors {
+  // Azul Petróleo (Cor principal para botões, destaques e hierarquia visual)
+  static const Color tealBlue = Color.fromARGB(255, 0, 77, 60);
+
+  // Amarelo Claro (Cor de fundo ou para elementos secundários que trazem leveza)
+  static const Color lightYellow = Color(0xFFFFFDE7);
+
+  // Cores Neutras
+  static const Color white = Colors.white;
+  static const Color black = Color.fromARGB(255, 0, 0, 0);
+  static const Color grey = Color.fromARGB(255, 128, 128, 128);
+
+  // Cores de Feedback (Acessibilidade)
+  static const Color success = Color(0xFF2E7D32); // Verde para aprovado na WCAG
+  static const Color warning = Color(
+    0xFFF9A825,
+  ); // Amarelo/Laranja para atenção
+  static const Color error = Color(
+    0xFFC62828,
+  ); // Vermelho para falha no contraste
+}
