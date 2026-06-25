8c16c69e6f18252d1314ab1f1bd1bfc6471e5965
Camila Valim Dias <camila.valim2004@gmail.com>
2026-06-25 17:13:36 -0300

    Fix: ContrastApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:pdm1_010526/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MeuApp());
+    await tester.pumpWidget(const ContrastApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
